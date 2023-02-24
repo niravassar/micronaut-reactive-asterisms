95704704170909986922e7f2898aed4fb3c95cf6
nassar <assarn@objectcomputing.com>
2023-02-24 13:38:36 -0600

    extend

--- src/main/java/com/reactive/beginner/Application.java
+++ src/main/java/com/reactive/beginner/Application.java
@@ -1,8 +1,9 @@
 package com.reactive.beginner;
 
+import io.asterisms.backend.core.application.AsterismsApplication;
 import io.micronaut.runtime.Micronaut;
 
-public class Application {
+public class Application extends AsterismsApplication {
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
