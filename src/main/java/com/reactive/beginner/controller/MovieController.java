ba3fffcf580be334d7e0d4af382fea874b26d6c3
nassar <assarn@objectcomputing.com>
2023-03-07 10:29:03 -0600

    try notify gandalf

--- src/main/java/com/reactive/beginner/controller/MovieController.java
+++ src/main/java/com/reactive/beginner/controller/MovieController.java
@@ -3,17 +3,30 @@ package com.reactive.beginner.controller;
 import com.reactive.beginner.entity.Actor;
 import com.reactive.beginner.entity.Movie;
 import com.reactive.beginner.service.MovieService;
+import io.asterisms.backend.core.notification.NotificationClient;
+import io.asterisms.core.account.AccountFetcherOperations;
+import io.asterisms.core.notifiable.NotifiableAccount;
+import io.asterisms.core.notification.Notification;
+import io.asterisms.core.notification.NotificationMessage;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import jakarta.inject.Inject;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 @Controller()
 public class MovieController {
 
     @Inject
     MovieService movieService;
+
+    @Inject
+    NotificationClient notificationClient;
+
+    @Inject
+    AccountFetcherOperations<NotifiableAccount> fetcherOperations;
 
     @Get("/hello")
     String sayHello() {
@@ -38,5 +51,22 @@ public class MovieController {
     @Get("/addJimCarreyToMovieAsActor")
     Mono<Movie> addJimCarreyToMovieAsActor() {
         return movieService.addJimCarreyToMovieAsActor().log();
+    }
+
+    @Get("/sendNotification")
+    void sendNotificaton() {
+        // send notification using hard coded UUID
+        UUID gandalfAccountId = UUID.fromString("1ad9d574-1ab8-4ec9-af63-034c314b8ccd");
+        fetcherOperations.findAccountById(gandalfAccountId)
+                .map(account -> {
+                    NotificationMessage message = new NotificationMessage();
+                    message.setSubject(account.getIdentity());
+                    message.setText("simple message body");
+
+                    Notification notification = new Notification(message, account);
+
+                    notificationClient.send(notification);
+                    return null;
+                });
     }
 }
