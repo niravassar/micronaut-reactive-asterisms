838c5d12489393039523ebbbce79b69daa20c812
nassar <assarn@objectcomputing.com>
2023-03-07 16:14:05 -0600

    actor in db

--- src/main/java/com/reactive/beginner/entity/Actor.java
+++ src/main/java/com/reactive/beginner/entity/Actor.java
@@ -1,13 +1,27 @@
 package com.reactive.beginner.entity;
 
+import io.micronaut.data.annotation.AutoPopulated;
+import io.micronaut.data.annotation.GeneratedValue;
+import io.micronaut.data.annotation.MappedEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Id;
+import java.util.UUID;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedEntity
 public class Actor {
+
+    @Id
+    @AutoPopulated
+    @GeneratedValue(GeneratedValue.Type.AUTO)
+    private UUID id;
+
     private String name;
+
     private Integer age;
 }
