7f600cba9c6714f63f9ca057ae50f4085fd44119
nassar <assarn@objectcomputing.com>
2023-03-07 16:14:05 -0600

    actor in db

---
+++ src/main/java/com/reactive/beginner/dataservice/ActorRepository.java
@@ -0,0 +1,15 @@
+package com.reactive.beginner.dataservice;
+
+import com.reactive.beginner.entity.Actor;
+import io.micronaut.data.model.query.builder.sql.Dialect;
+import io.micronaut.data.r2dbc.annotation.R2dbcRepository;
+import io.micronaut.data.repository.reactive.ReactorCrudRepository;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
+
+@R2dbcRepository(dialect = Dialect.POSTGRES)
+public interface ActorRepository extends ReactorCrudRepository<Actor, UUID> {
+
+    Flux<Actor> findByName(String name);
+}
---
+++ src/main/resources/db/migration/V001__create_actor_database.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE IF NOT EXISTS actor (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(128) NOT NULL unique,
+    age integer NOT NULL,
+);
